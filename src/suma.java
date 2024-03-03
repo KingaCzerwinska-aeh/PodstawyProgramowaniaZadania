f38bbf4e225ec318a44e245c4c0aa2a2dadfe22b
Kinga <czerwki9515_aehit@students.vizja.pl>
2024-03-03 10:22:03 +0100

    Zadnia1 wyswietlanie sumy liczb

---
+++ src/suma.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class suma {
+    public static void main(String[] args) {
+       int x = 7;
+       int y = 5;
+       int sum = x + y;
+       System.out.println("Sum:" + sum);
+        }
+    }
